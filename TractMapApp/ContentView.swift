04ef7eb2a514a59fb1defcfdad4c9becaf390877
Jayden Metz <realtor@jaydenmetz.com>
2024-11-07 00:03:32 -0800

    Restart simple

--- TractMapApp/ContentView.swift
+++ TractMapApp/ContentView.swift
@@ -8,32 +8,67 @@
 import SwiftUI
 import MapKit
 
-import SwiftUI
-import MapKit
-
 struct ContentView: View {
-    @ObservedObject var viewModel = MapViewModel()
+    @StateObject private var locationManager = LocationManager()
+    
+    // State for the map's camera position
+    @State private var cameraPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), // Default to San Francisco
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+    )
 
     var body: some View {
-        VStack {
-            Map(coordinateRegion: $viewModel.region,
-                interactionModes: .all,
-                showsUserLocation: true,
-                annotationItems: viewModel.annotations) { annotation in
-                MapAnnotation(coordinate: annotation.coordinate) {
-                    Text(annotation.title ?? "")
-                        .font(.caption)
-                        .foregroundColor(.black)
-                        .padding(5)
-                        .background(Color.white)
-                        .clipShape(Capsule())
+        ZStack {
+            Map(position: $cameraPosition) {
+                // Use an Annotation for the user's current location
+                if let location = locationManager.currentLocation {
+                    Annotation("Current Location", coordinate: location.coordinate) {
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 12, height: 12)
+                    }
                 }
             }
-            .edgesIgnoringSafeArea(.all)
+            .mapControls {
+                MapUserLocationButton() // Add a button to recenter on the user's location
+                MapCompass() // Add a compass control
+            }
+            .ignoresSafeArea()
+            .onAppear {
+                locationManager.requestLocation() // Request the user's location on appear
+            }
+            .onChange(of: locationManager.currentLocation) { newLocation in
+                if let location = newLocation {
+                    updateCameraPosition(location)
+                }
+            }
+
+            // Show a message if location access is denied
+            if locationManager.authorizationStatus == .denied || locationManager.authorizationStatus == .restricted {
+                Text("Location access denied. Please enable it in Settings.")
+                    .padding()
+                    .background(Color.white.opacity(0.8))
+                    .cornerRadius(10)
+                    .padding()
+            }
         }
+    }
+
+    // Update the camera position to center on the user's location
+    private func updateCameraPosition(_ location: CLLocation) {
+        cameraPosition = .region(
+            MKCoordinateRegion(
+                center: location.coordinate,
+                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            )
+        )
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ TractMapApp/LocationManager.swift
@@ -0,0 +1,50 @@
+//
+//  LocationManager.swift
+//  TractMapApp
+//
+//  Created by Jayden Metz on 11/6/24.
+//
+
+import Foundation
+import CoreLocation
+import Combine
+import MapKit
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    
+    @Published var currentLocation: CLLocation?
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        requestPermission()
+    }
+
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    func requestLocation() {
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        DispatchQueue.main.async {
+            self.currentLocation = location
+        }
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        DispatchQueue.main.async {
+            self.authorizationStatus = manager.authorizationStatus
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get user location: \(error.localizedDescription)")
+    }
+}
